4853e8c16751d7c2b4314da25a44a5551320c34d
Konstantin Zelmanovich <Constantine19>
2024-06-02 15:29:17 +0300

    add nested projects

--- new-team/main.tf
+++ new-team/main.tf
@@ -1,5 +1,3 @@
-# Terraform 0.13+ uses the Terraform Registry:
-
 terraform {
   required_providers {
     env0 = {
@@ -8,14 +6,22 @@ terraform {
   }
 }
 
-# Configure the env0 provider
-
 provider "env0" {
   api_key    = var.env0_api_key
   api_secret = var.env0_api_secret
 }
 
-resource "env0_project" "kosta" {
-  name        = "kosta"
-  description = "Example project"
+resource "env0_project" "team_project" {
+  name        = var.team_name
+  description = "Team project for ${var.team_name}"
+
+  policies = var.policies
+}
+
+resource "env0_project" "environment_projects" {
+  for_each = toset(var.team_environments)
+
+  name        = "${var.team_name}-${each.value}"
+  description = "${each.value} environment for ${var.team_name} project"
+  parent_project_id = env0_project.team_project.id
 }
