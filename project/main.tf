d7cc4d97a86da5135a10dac26cc1d39eb8a47244
Konstantin Zelmanovich <Constantine19>
2024-06-02 14:55:53 +0300

    initial

---
+++ project/main.tf
@@ -0,0 +1,21 @@
+# Terraform 0.13+ uses the Terraform Registry:
+
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+    }
+  }
+}
+
+# Configure the env0 provider
+
+provider "env0" {
+  api_key    = var.env0_api_key
+  api_secret = var.env0_api_secret
+}
+
+resource "env0_project" "kosta" {
+  name        = "kosta"
+  description = "Example project"
+}
---
+++ project/variables.tf
@@ -0,0 +1,9 @@
+variable "env0_api_key" {
+  description = "API key for env0"
+  type        = string
+}
+
+variable "env0_api_secret" {
+  description = "API secret for env0"
+  type        = string
+}
